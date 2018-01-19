7d9dfff305b8342637008558f9ba803dbba567f6
Erik Dietrich <erik@daedtech.com>
2018-01-18 22:37:35 -0800

    Getting the ball rolling with a feedly inquistor

---
+++ ElDorado.Console.Tests/Class1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElDorado.Console.Tests
+{
+    [TestClass]
+    public class When_Searching_For_Subscriber_Count_FeedlyInquisitor_Should
+    {
+        [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+        public void Return_0_By_Default()
+        {
+            var inquisitor = new FeedlyInquisitor();
+
+            //var count = inquisitor.GetSubscriberCount("http://cloud.feedly.com/v3/feeds/feed%2Fhttp%3A%2F%2Fwww.daedtech.com%2Ffeed");
+            var count = inquisitor.GetSubscriberCount(string.Empty);
+
+            count.ShouldBe(0);
+        }
+}
+    public class FeedlyInquisitor
+    {
+
+        public int GetSubscriberCount(string str)
+        {
+            return 0;
+        }
+    }
+}
---
+++ ElDorado.Console.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ElDorado.Console.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ElDorado.Console.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("568db9d0-7f06-4d45-962a-e526d28f4937")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
