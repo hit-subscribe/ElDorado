6be28f2f926eb06a33298934a4cdf44fc3500e64
Erik Dietrich <erik@daedtech.com>
2018-04-05 15:28:12 -0700

    Implemented a menu structure to simulate what might eventually be a GUI

---
+++ ElDorado/Menu/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using ElDorado.Domain;
+using ElDorado.Metrics;
+using ElDorado.WritingCalendar;
+using Gold.ConsoleMenu;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElDorado.Menu
+{
+    [MenuClass("Main Menu")]
+    public class MainMenu
+    {
+        [MenuMethod("Record blog metrics")]
+        public static void RecordMetrics()
+        {
+            var webClient = new SimpleWebClient();
+            var feedlyInquisitor = new FeedlyInquisitor(webClient);
+            var alexaInquisitor = new AlexaDataInquisitor(webClient);
+            var mozInquisitor = new MozInquisitor(new CredentialStore(File.ReadAllText(@"CredFiles\moz.cred")), webClient) { Timeout = 10 };
+
+            var metricsRecorder = new BlogMetricsRecorder(feedlyInquisitor, alexaInquisitor, mozInquisitor);
+
+            using (var context = new BlogContext())
+            {
+                var blogs = context.Blogs.ToList();
+                var blogMetrics = metricsRecorder.GenerateMetrics(blogs);
+                context.BlogMetrics.AddRange(blogMetrics);
+                context.SaveChanges();
+            }
+        }
+
+        [MenuMethod("Update Trello based on Post Plan")]
+        public static void UpdateTrelloWritingCalendar()
+        {
+            var trelloService = new TrelloWritingCalendarService();
+            trelloService.Initialize(new CredentialStore(File.ReadAllText(@"CredFiles\trello.cred")));
+
+            var synchornizer = new BlogPostSynchronizer(trelloService, new PlanningSpreadsheetService());
+            synchornizer.UpdatePlannedInTrello();
+        }
+    }
+}
--- ElDorado/Program.cs
+++ ElDorado/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ElDorado.Domain;
 using ElDorado.Metrics;
 using ElDorado.WritingCalendar;
+using Gold.ConsoleMenu;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
@@ -24,110 +25,7 @@ namespace ElDorado
     {
         static void Main(string[] args)
         {
-            //GenerateBlogMetrics();
-
-            Synchronize();
-
-            //SetupTrelloConnection();
-            //AddCardToTrello();
-            //ReadFromGoogleSheet();
-        }
-
-        private static void GenerateBlogMetrics()
-        {
-            var webClient = new SimpleWebClient();
-            var feedlyInquisitor = new FeedlyInquisitor(webClient);
-            var alexaInquisitor = new AlexaDataInquisitor(webClient);
-            var mozInquisitor = new MozInquisitor(new CredentialStore(File.ReadAllText(@"CredFiles\moz.cred")), webClient) { Timeout = 10 };
-
-            var metricsRecorder = new BlogMetricsRecorder(feedlyInquisitor, alexaInquisitor, mozInquisitor);
-
-            using (var context = new BlogContext())
-            {
-                var blogs = context.Blogs.ToList();
-                var blogMetrics = metricsRecorder.GenerateMetrics(blogs);
-                context.BlogMetrics.AddRange(blogMetrics);
-                context.SaveChanges();
-            }
-        }
-
-        private static void Synchronize()
-        {
-            var trelloService = new TrelloWritingCalendarService();
-            trelloService.Initialize(new CredentialStore(File.ReadAllText(@"CredFiles\trello.cred")));
-
-            var synchornizer = new BlogPostSynchronizer(trelloService, new PlanningSpreadsheetService());
-            synchornizer.UpdatePlannedInTrello();
-        }
-
-        private static void SetupTrelloConnection()
-        {
-            var credentials = new CredentialStore(File.ReadAllText(@"CredFiles\trello.cred"));
-            var serializer = new ManateeSerializer();
-            TrelloConfiguration.Serializer = serializer;
-            TrelloConfiguration.Deserializer = serializer;
-            TrelloConfiguration.JsonFactory = new ManateeFactory();
-            TrelloConfiguration.RestClientProvider = new WebApiClientProvider();
-            TrelloAuthorization.Default.AppKey = credentials["TrelloAppKey"];
-            TrelloAuthorization.Default.UserToken = credentials["TrelloUserToken"]; 
-        }
-
-        private static void AddCardToTrello()
-        {
-            var board = new Board("AhqnpUJD");
-            var plannedPostsList = board.Lists.First(l => l.Name == "Planned Posts");
-            plannedPostsList.Cards.Add("A fifth added from El Dorado.");
-        }
-
-        private static void ReadFromGoogleSheet()
-        {
-            // Create Google Sheets API service.
-            var service = new SheetsService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = GetSheetsUserCredential(),
-                ApplicationName = "El Dorado",
-            });
-
-            var spreadsheetId = "1BFycG-T2eY3Uh8HWr5c5h-MjYEUJ8eKjqJ8GLxhdh2w";
-            var range = "Current!A2:E";
-            var request = service.Spreadsheets.Values.Get(spreadsheetId, range);
-
-            var values = request.Execute().Values;
-            Pad(values);
-
-            Console.WriteLine("Client, Title");
-            foreach (var row in values.Where(r => r.Count > 0))
-                Console.WriteLine("{0}, {1}", row[0], row[1]);
-
-            Console.Read();
-        }
-
-        private static UserCredential GetSheetsUserCredential()
-        {
-            // If modifying these scopes, delete your previously saved credentials
-            // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-            UserCredential credential;
-
-            using (var stream = new FileStream(@"CredFiles\google.cred", FileMode.Open, FileAccess.Read))
-            {
-                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
-
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, new string[] { SheetsService.Scope.SpreadsheetsReadonly }, "user", CancellationToken.None, new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
-
-            return credential;
-        }
-
-        private static void Pad(IList<IList<object>> target)
-        {
-            var maxColumns = target.Max(r => r.Count);
-            foreach(var row in target)
-            {
-                for (int i = row.Count; i < maxColumns; i++)
-                    row.Add(string.Empty);
-            }
+            MenuGenerator.Run();
         }
     }
 }
