44dd10dcae73f881566cb6c05453f25ff1e55b34
Erik Dietrich <erik@daedtech.com>
2018-04-19 22:32:59 -0700

    Added concept of a client spreadsheet that we'll eventually sync.

---
+++ ElDorado.Console.Tests/WritingCalendar/When_Writing_To_A_ClientPostPlan_ClientSpreadsheetService_Should.cs
@@ -0,0 +1,57 @@
+﻿using ElDorado.Domain;
+using ElDorado.WritingCalendar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.JustMock;
+using Telerik.JustMock.Helpers;
+
+namespace ElDorado.Console.Tests.WritingCalendar
+{
+    [TestClass]
+    public class When_Writing_To_A_ClientPostPlan_ClientSpreadsheetService_Should
+    {
+        private const string Title = "A Blog Post";
+        private const string Mission = "To boldly go...";
+        private static readonly DateTime PubDate = new DateTime(2018, 12, 1);
+
+        private BlogPost Post { get; } = new BlogPost() { Title = Title, Mission = Mission, TargetPublicationDate = PubDate };
+
+        private GoogleSheet Sheet { get; } = Mock.Create<GoogleSheet>();
+
+        private ClientSpreadsheetService Target { get; set; }
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            Target = new ClientSpreadsheetService(Sheet);
+        }
+
+        [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+        public void Set_Column_0_To_Title()
+        {
+            Target.AddPosts(Post.AsEnumerable());
+
+            Sheet.Assert(s => s.UpdateSpreadsheet(Arg.AnyString, Arg.Matches<IList<IList<object>>>(grid => grid[0][0].ToString() == Title)));
+        }
+
+        [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+        public void Set_Column_3_To_Mission()
+        {
+            Target.AddPosts(Post.AsEnumerable());
+
+            Sheet.Assert(s => s.UpdateSpreadsheet(Arg.AnyString, Arg.Matches<IList<IList<object>>>(grid => grid[0][3].ToString() == Mission)));
+        }
+
+        [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+        public void Set_Column_4_To_PublicationDate()
+        {
+            Target.AddPosts(Post.AsEnumerable());
+
+            Sheet.Assert(s => s.UpdateSpreadsheet(Arg.AnyString, Arg.Matches<IList<IList<object>>>(grid => grid[0][4].ToString() == PubDate.ToString())));
+        }
+    }
+}
---
+++ ElDorado/WritingCalendar/ClientSpreadsheetService.cs
@@ -0,0 +1,31 @@
+﻿using ElDorado.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElDorado.WritingCalendar
+{
+    public class ClientSpreadsheetService
+    {
+        private readonly GoogleSheet _clientSheet;
+        public ClientSpreadsheetService(GoogleSheet googleSheet)
+        {
+            _clientSheet = googleSheet;
+        }
+
+        public void AddPosts(IEnumerable<BlogPost> posts)
+        {
+            IList<IList<object>> rows = new List<IList<object>>();
+
+            foreach (var post in posts)
+            {
+                IList<object> columnValues = new List<object>() { post.Title, string.Empty, string.Empty, post.Mission, post.TargetPublicationDate };
+                rows.Add(columnValues);
+            }
+
+            _clientSheet.UpdateSpreadsheet("asdf", rows);
+        }
+    }
+}
