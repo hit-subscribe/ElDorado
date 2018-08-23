44186fd231617e34ccb7617eb05f94cfe645e605
Erik Dietrich <erik@daedtech.com>
2018-08-22 23:33:47 -0400

    Added CRUD for the blogs.

---
+++ ElDorado.Gui/Controllers/BlogsController.cs
@@ -0,0 +1,13 @@
+﻿using ElDorado.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElDorado.Gui.Controllers
+{
+    public class BlogsController : CrudController<Blog>
+    {
+        public BlogsController(BlogContext context) : base(context) { }
+    }
+}
